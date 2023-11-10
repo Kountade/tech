ce093b058b825b3723a6d9108f29abd6383fd609
Bounama Kounta <bounamakountagoudiaby@gmail.com>
2023-11-10 15:27:15 +0000

    first commit

---
+++ solution/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from solution import views
+urlpatterns = [
+    path("", views.home, name="home"),
+    path("blogs", views.blogs, name="blogs"),
+    path("services", views.service, name="services"),
+    path("pricing", views.pricing, name="pricing"),
+    path("contact", views.contact, name="contact"),
+    path("blogs/blogdetail/<str:title>", views.blogdetail, name="blogdetail"),
+    path("register", views.register, name="register"),
+  
+    
+]
